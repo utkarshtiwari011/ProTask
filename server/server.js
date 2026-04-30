b441f3e8f0a0a56f083399d0db78ce91798344bc
Utkarsh Tiwari <utkarshtiwari3214@gmail.com>
2026-05-01 04:33:43 +0530

    Fix Express 5 wildcard routing crash

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const distPath = path.join(__dirname, '../client/dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
 
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 } else {
